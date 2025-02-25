e3af3439fc7059c197714630d317e91d6d9cc89f
Алена Новикова <novikovaaaleksa@intech.rshb.ru>
2025-02-25 16:40:00 +0300

    update ValidationPipe option

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
         timestamp: true,
       }),
     });
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     const documentFactory = () =>
       SwaggerModule.createDocument(app, configSwagger, optionsSwagger);
     SwaggerModule.setup('swagger', app, documentFactory, {

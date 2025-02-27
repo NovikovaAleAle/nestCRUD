f588f1f074334610350e51abbe8eeafd510d0044
NovikovaAleAle <novikova.ale.ale@gmail.com>
2025-02-27 13:26:34 +0300

    update cofig

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,7 +3,7 @@ import { User } from '../users/user.entity';
 
 export const dbConfig: DataSourceOptions = {
   type: 'postgres',
-  host: '172.21.172.187',
+  host: 'localhost',
   port: 5432,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,

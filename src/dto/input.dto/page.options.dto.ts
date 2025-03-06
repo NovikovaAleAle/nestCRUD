55c05e5ef9690315511df2c610b24aec10ef5c8f
Алена Новикова <novikovaaaleksa@intech.rshb.ru>
2025-03-06 18:14:43 +0300

    added IsNumberAndStringMatch decorator

--- src/dto/input.dto/page.options.dto.ts
+++ src/dto/input.dto/page.options.dto.ts
@@ -1,17 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsInt, Min } from 'class-validator';
+import { IsOptional } from 'class-validator';
+import { IsNumberAndStringMatch } from 'src/decorators/validation.decorators';
 
 export class PageOptionsDto {
-  @ApiProperty({ description: 'Number page', default: 1 })
-  @IsInt()
-  @Min(1)
+  @ApiProperty({ description: 'Number page', minimum:1, default: 1 })
   @IsOptional()
+  @IsNumberAndStringMatch(1,{
+    message:'page does not match the given pattern',
+  })
   readonly page: number = 1;
 
-  @ApiProperty({ description: 'Number of records per page', default: 3 })
-  @IsInt()
-  @Min(3)
+  @ApiProperty({ description: 'Number of records per page', minimum:3, default: 3 })
   @IsOptional()
+  @IsNumberAndStringMatch(3,{
+    message:'take does not match the given pattern ',
+  },)
   readonly take: number = 3;
 
   get skip(): number {
